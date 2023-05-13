21ed1824b2ba519168e93186e84220dd4c59e521
fhctyw <lisnychukarsenw@gmail.com>
2023-05-13 15:37:45 +0300

    update add dynamo_table in backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "dev-lab-website"
-    key    = "./terraform.tfstate"
-    region = "eu-central-1"
+    bucket         = "dev-lab-website"
+    key            = "./terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-tfstate-lock"
   }
 }
